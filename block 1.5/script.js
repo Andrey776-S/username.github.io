43e911036b7eff6cc66df1ddf66f5ba60f9d2634
Andrey776-S <83342935+Andrey776-S@users.noreply.github.com>
2021-11-17 20:04:28 +0500

    block 1.5

---
+++ block 1.5/script.js
@@ -0,0 +1,31 @@
+new Swiper('.swiper', {
+    slidesPerView: 1.2,
+    spaceBetween: 16,
+
+    // centerSlides: true,
+
+    // autoHeight: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    }
+});
+
+let buttonOpen = document.querySelector('.button-show');
+let buttonClose = document.querySelector('.button-close');
+let block = document.querySelector('.block');
+
+buttonOpen.addEventListener('click', function (evt){
+    evt.preventDefault();
+    buttonOpen.classList.add('class-hidden');
+    buttonClose.classList.add('class-show');
+    block.classList.add('class-overflowVisible');
+})
+
+buttonClose.addEventListener('click', function (evt){
+    evt.preventDefault();
+    buttonOpen.classList.remove('class-hidden');
+    buttonClose.classList.remove('class-show');
+    block.classList.remove('class-overflowVisible');
+});
